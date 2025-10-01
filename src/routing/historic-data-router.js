79f9a6ce2b09073d55039fbbd987229d0a76f3ec
Damian Gasiorowski <softwaredevelopment.dg@gmail.com>
2025-10-01 11:23:09 +0200

    [crypto-exchange] binance service but with errors

--- src/routing/historic-data-router.js
+++ src/routing/historic-data-router.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
+import { fetchHistoricData } from "../services/binance.js";
 
 const router = Router();
 
-router.get("/historic-data", (req, res) => {
-  const symbol = req.query.symbol;
-  res.send(`Historic data endpoint for ${symbol}`);
+router.get("/historic-data", async (req, res) => {
+  const { symbol, from, to } = req.query;
+  const fromInMilis = new Date(from).getTime();
+  const toInMilis = new Date(to).getTime();
+
+  console.log({ symbol, fromInMilis, toInMilis });
+  const results = await fetchHistoricData(symbol, fromInMilis, toInMilis);
+  res.json({
+    description: `Historic data endpoint for ${symbol}`,
+    data: results,
+  });
 });
 
 export const historicDataRouter = router;
---
+++ src/services/binance.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.binance.com/";
+
+const INTERVAL = "1h"; // 1 minute interval
+
+export const fetchHistoricData = async (symbol, from, to) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/api/v3/klines`, {
+      params: {
+        symbol,
+        interval: INTERVAL,
+      },
+    });
+
+    return response;
+  } catch (error) {
+    console.error("Error fetching historic data from Binance:", error);
+    return null;
+  }
+};
