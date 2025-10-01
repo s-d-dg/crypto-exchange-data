b3140dde9bde9c7815bea07bb1916dc0cf11a638
Damian Gasiorowski <softwaredevelopment.dg@gmail.com>
2025-10-01 11:33:15 +0200

    [crypto-exchange] added time range params

--- src/routing/historic-data-router.js
+++ src/routing/historic-data-router.js
@@ -8,21 +8,10 @@ router.get("/historic-data", async (req, res) => {
   const fromInMilis = new Date(from).getTime();
   const toInMilis = new Date(to).getTime();
 
-  console.log({ symbol, fromInMilis, toInMilis });
   const results = await fetchHistoricData(symbol, fromInMilis, toInMilis);
   res.json({
     description: `Historic data endpoint for ${symbol}`,
     data: results.data,
-  });
-});
-
-router.get("/avgPrice", async (req, res) => {
-  const { symbol } = req.query;
-
-  const result = await avgPrice(symbol);
-  res.json({
-    description: `Average price endpoint for ${symbol}`,
-    data: result,
   });
 });
 
--- src/services/binance.js
+++ src/services/binance.js
@@ -10,27 +10,14 @@ export const fetchHistoricData = async (symbol, from, to) => {
       params: {
         symbol,
         interval: INTERVAL,
+        startTime: from,
+        endTime: to,
       },
     });
 
     return response;
   } catch (error) {
     console.error("Error fetching historic data from Binance:", error);
-    return null;
-  }
-};
-
-export const avgPrice = async (symbol) => {
-  try {
-    const response = await axios.get(`${BASE_URL}/api/v3/avgPrice`, {
-      params: {
-        symbol,
-      },
-    });
-
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching average price from Binance:", error);
     return null;
   }
 };
